d9de39259def93b23b4cc48cafcd8e1d6b47bf91
Julian Bautista Cruz <julian.bautista@uabc.edu.mx>
2022-05-31 00:00:46 -0700

    Inicializamos el programa, e importamos librerias aunque algunos no funcionan aun

---
+++ login_rostro.py
@@ -0,0 +1,10 @@
+##importando librerias
+
+from tkinter import*
+import os
+##import cv2
+from matplotlib import pyplot
+##from mtcnn.mtcnn import MTCNN
+import numpy as np
+
+## se crea una funcion para registrar al usuario
